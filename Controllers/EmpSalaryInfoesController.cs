bcabf392804275cdd391e6c620b278f9280c630f
agarwal30shubham@gmail.com <agarwal30shubham@gmail.com>
2021-06-12 01:45:48 -0700

    Second Commit

---
+++ Controllers/EmpSalaryInfoesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoreMVCWithAzureDatabase.Models;
+
+namespace CoreMVCWithAzureDatabase.Controllers
+{
+    public class EmpSalaryInfoesController : Controller
+    {
+        private readonly EmpDatabaseContext _context;
+
+        public EmpSalaryInfoesController(EmpDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EmpSalaryInfoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.EmpSalaryInfo.ToListAsync());
+        }
+
+        // GET: EmpSalaryInfoes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empSalaryInfo = await _context.EmpSalaryInfo
+                .FirstOrDefaultAsync(m => m.PayBand == id);
+            if (empSalaryInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(empSalaryInfo);
+        }
+
+        // GET: EmpSalaryInfoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmpSalaryInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PayBand,BasicSalary")] EmpSalaryInfo empSalaryInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(empSalaryInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empSalaryInfo);
+        }
+
+        // GET: EmpSalaryInfoes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empSalaryInfo = await _context.EmpSalaryInfo.FindAsync(id);
+            if (empSalaryInfo == null)
+            {
+                return NotFound();
+            }
+            return View(empSalaryInfo);
+        }
+
+        // POST: EmpSalaryInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PayBand,BasicSalary")] EmpSalaryInfo empSalaryInfo)
+        {
+            if (id != empSalaryInfo.PayBand)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(empSalaryInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpSalaryInfoExists(empSalaryInfo.PayBand))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empSalaryInfo);
+        }
+
+        // GET: EmpSalaryInfoes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empSalaryInfo = await _context.EmpSalaryInfo
+                .FirstOrDefaultAsync(m => m.PayBand == id);
+            if (empSalaryInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(empSalaryInfo);
+        }
+
+        // POST: EmpSalaryInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var empSalaryInfo = await _context.EmpSalaryInfo.FindAsync(id);
+            _context.EmpSalaryInfo.Remove(empSalaryInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpSalaryInfoExists(string id)
+        {
+            return _context.EmpSalaryInfo.Any(e => e.PayBand == id);
+        }
+    }
+}
---
+++ Models/EmpDatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CoreMVCWithAzureDatabase.Models
+{
+    public partial class EmpDatabaseContext : DbContext
+    {
+        public EmpDatabaseContext()
+        {
+        }
+
+        public EmpDatabaseContext(DbContextOptions<EmpDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmpSalaryInfo> EmpSalaryInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:shubhamnewserver.database.windows.net,1433;Initial Catalog=EmpDatabase;Persist Security Info=False;User ID=shubham12345;Password=shubham@12345;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmpSalaryInfo>(entity =>
+            {
+                entity.HasKey(e => e.PayBand)
+                    .HasName("PK__EmpSalar__66B0F53FD7013562");
+
+                entity.Property(e => e.PayBand).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
