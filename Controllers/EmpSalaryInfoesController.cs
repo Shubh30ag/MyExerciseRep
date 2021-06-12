ec9bcb9d8149597661f3460afa4b9bc9443eac1a
agarwal30shubham@gmail.com <agarwal30shubham@gmail.com>
2021-06-12 03:00:18 -0700

    Final Commit

--- Controllers/EmpSalaryInfoesController.cs
+++ Controllers/EmpSalaryInfoesController.cs
@@ -21,6 +21,7 @@ namespace CoreMVCWithAzureDatabase.Controllers
         // GET: EmpSalaryInfoes
         public async Task<IActionResult> Index()
         {
+            
             return View(await _context.EmpSalaryInfo.ToListAsync());
         }
 
